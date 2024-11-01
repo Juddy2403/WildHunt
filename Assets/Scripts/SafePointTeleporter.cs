ea314874b80315d60de62877cc3b9dc0ae198d13
Juddy2403 <ioanaraileanu24@yahoo.com>
2024-11-01 17:37:26 +0100

    faet: player can change in/out scenes

---
+++ Assets/Scripts/SafePointTeleporter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SafePointTeleporter : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset _inputAsset;
+    private InputAction _interactAction;
+    private bool _isOnSafePoint = false;
+    void Awake()
+    {
+        if (!_inputAsset) return;
+
+        //Bind the actions to the input asset
+        _interactAction = _inputAsset.FindActionMap("Gameplay").FindAction("Interact");
+
+        //we bind a callback to it instead of continiously monitoring input
+        _interactAction.performed += HandleInteraction;
+    }
+    void OnDestroy()
+    {
+        _interactAction.performed -= HandleInteraction;
+    }
+    void HandleInteraction(InputAction.CallbackContext context)
+    {
+        if (!_isOnSafePoint) return;
+        if (SceneManager.GetActiveScene().name == "Outside")
+            SceneManager.LoadScene("Inside");
+        else SceneManager.LoadScene("Outside");
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "SafePointCollider") _isOnSafePoint = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "SafePointCollider") _isOnSafePoint = false;
+    }
+}
