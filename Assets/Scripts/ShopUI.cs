33533ce91e160bb04de43d20c7e2ffa9fa184f41
Juddy2403 <ioanaraileanu24@yahoo.com>
2024-11-09 16:51:19 +0100

    feat: shop ui remembers values

--- Assets/Scripts/ShopUI.cs
+++ Assets/Scripts/ShopUI.cs
@@ -8,51 +8,62 @@ public class ShopUI : MonoBehaviour
 {
     private UIDocument _attachedDocument = null;
     private VisualElement _root = null;
-    private Upgrade _hpUpgrade;
-    private Upgrade _gunUpgrade;
-    private Upgrade _knifeUpgrade;
-    private Upgrade _movementUpgrade;
-    
-    private struct Upgrade
+    private Upgrade _hpUpgrade = null;
+    private Upgrade _gunUpgrade = null;
+    private Upgrade _knifeUpgrade = null;
+    private Upgrade _movementUpgrade = null;
+
+    private class Upgrade
     {
         public string _name;
         private Button _button;
         private ProgressBar _progressBar;
+        private float _progressBarValue;
         private Label _costLabel;
+        private int _costLabelValue;
 
-        public Upgrade(VisualElement root,string name)
+        public Upgrade(VisualElement root, string name)
         {
             _name = name;
             _button = root.Q<Button>(name + "UpgradeButton");
             _progressBar = root.Q<ProgressBar>(name + "UpgradeBar");
             _costLabel = root.Q<Label>(name + "UpgradePrice");
+            _progressBarValue = 0f;
+            _costLabelValue = 20;
             _button.clickable.clicked += OnUpgrade;
         }
-        
+
+        public void Update(VisualElement root)
+        {
+            _button = root.Q<Button>(_name + "UpgradeButton");
+            _progressBar = root.Q<ProgressBar>(_name + "UpgradeBar");
+            _progressBar.value = _progressBarValue;
+            _costLabel = root.Q<Label>(_name + "UpgradePrice");
+            _costLabel.text = _costLabelValue.ToString();
+            _button.clickable.clicked += OnUpgrade;
+        }
+
         private void OnUpgrade()
         {
             Debug.Log("Upgrading " + _name);
             _progressBar.value += 10f;
-            int cost = Convert.ToInt32(_costLabel.text);
-            cost += 10;
-            _costLabel.text = cost.ToString();
+            _progressBarValue += 10f;
+            _costLabelValue += 10;
+            _costLabel.text = _costLabelValue.ToString();
         }
     }
+
     private void OnEnable()
     {
         _attachedDocument = GetComponent<UIDocument>();
         if (_attachedDocument) _root = _attachedDocument.rootVisualElement;
-        _hpUpgrade = new Upgrade(_root, "Hp");
-        _gunUpgrade = new Upgrade(_root, "Gun");
-        _knifeUpgrade = new Upgrade(_root, "Knife");
-        _movementUpgrade = new Upgrade(_root, "Movement");
-        
+        if (_hpUpgrade == null) _hpUpgrade = new Upgrade(_root, "Hp");
+        else _hpUpgrade.Update(_root);
+        if (_gunUpgrade == null) _gunUpgrade = new Upgrade(_root, "Gun");
+        else _gunUpgrade.Update(_root);
+        if (_knifeUpgrade == null) _knifeUpgrade = new Upgrade(_root, "Knife");
+        else _knifeUpgrade.Update(_root);
+        if (_movementUpgrade == null) _movementUpgrade = new Upgrade(_root, "Movement");
+        else _movementUpgrade.Update(_root);
     }
-
-    private void OnHpUpgrade()
-    {
-        //upgrade the player's health
-        Debug.Log("Upgrading health");
-    }
-    
 }
