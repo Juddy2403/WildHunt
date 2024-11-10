2185dffe49175d3db0e957edd26696cb1ff2f68a
Juddy2403 <ioanaraileanu24@yahoo.com>
2024-11-10 23:08:57 +0100

    feat: text popup fades out

--- Assets/Scripts/Managers/TextPopup.cs
+++ Assets/Scripts/Managers/TextPopup.cs
@@ -8,6 +8,8 @@ public class TextPopup : SingletonBase<TextPopup>
     private UIDocument _attachedDocument = null;
     private VisualElement _root = null;
     private Label _text = null;
+    private const float _fadeTime = 0.5f;
+
     private void Start()
     {
         _attachedDocument = GetComponent<UIDocument>();
@@ -20,12 +22,27 @@ public class TextPopup : SingletonBase<TextPopup>
 
         if (_root == null) return;
         _text = _root.Q<Label>();
+        _text.style.opacity = 1;
         _text.text = text;
-        Invoke(nameof(DisableText),1);
+        Invoke(nameof(FadeText),1);
     }
-    
-    private void DisableText()
+    private void FadeText()
     {
+        StartCoroutine(FadeTextCoroutine());
+    }
+    private IEnumerator FadeTextCoroutine()
+    {
+        float time = _fadeTime;
+
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            var normalizedTime = Mathf.Clamp01(time / _fadeTime);
+            _text.style.opacity = normalizedTime;
+            yield return new WaitForEndOfFrame();//this loop will continue next frame until the while loop has finished
+        }
+        //ensure we are the starting color again at the end if we would not exactly hit it due to rounding
         _attachedDocument.enabled = false;
+
     }
 }
